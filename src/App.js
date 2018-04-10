d44e5e5959c0a519973a78091a1ac9e8ab52b2f2
Lucas Alves <lucas.alves.costa96@gmail.com>
2018-04-10 18:48:58 -0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FragmentoVital from "./FragmentoVital";
+import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div>
+              <h1>EMERSON DA SILVA</h1>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/fragmentovital">Fragmento Vital</Link>
+                  </li>
+                  <li>Adaptação reversa</li>
+                  <li>Espécie</li>
+                </ul>
+              </nav>
+            </div>
+          )}
+        />
+        <Route
+          path="/fragmentovital"
+          render={({ history }) => <FragmentoVital />}
+        />
       </div>
     );
   }
