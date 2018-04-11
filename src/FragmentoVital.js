a4b48026171dc2aa2b4449177a600e73fa729b39
Lucas <lucas.alves.costa96@gmail.com>
2018-04-11 00:31:40 -0300

    Test url

--- src/FragmentoVital.js
+++ src/FragmentoVital.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 class FragmentoVital extends Component {
   render(){
     let fragmentogallery = [
-        "1",
-        "2",
-        "3",
-        "4"
+        "https://www.seoclerk.com/pics/558390-11FO8A1505384509.png",
+        "http://brandmark.io/logo-rank/random/pepsi.png",
+        "https://image.freepik.com/icones-gratis/xbox-logotipo_318-9975.jpg",
+        "https://www.brachaprinting.com/wp-content/uploads/2013/10/Apple-logo1.jpg"
     ]
     return(
       <div>
         <Link to="/">Close</Link>
         {fragmentogallery.map((url) => {
           return (
-          <p>{url}</p>  
+          <img src={url}></img>
           )
         })}
       </div>
