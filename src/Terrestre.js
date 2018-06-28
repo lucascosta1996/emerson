a4d5d5df34c7f7d9008efeb69d7ea00e1012f814
Lucas Alves <lucas.alves.costa96@gmail.com>
2018-06-27 22:38:17 -0300

    Update Terrestre.js

--- src/Terrestre.js
+++ src/Terrestre.js
@@ -1,35 +1,61 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 class Terrestre extends Component {
-  render(){
-    let terrestregallery = [
-        "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12", "T13", "T14", "T15", "T16", "T17", "T18"
-    ]
-    return (
-          <div>
-            <section class="galleries">
-              <div id="terrestreTitle">
-                <h2>TERRESTRE</h2>
-              </div>
-              {terrestregallery.map((url, index) => {
-                return <img src={require(`./terrestre/${url}.jpg`)} key={index} />;
-              })}
-            </section>
-            <div class="expoDescription">
-              <a href="#" class="release">
-                release
-              </a>
-            </div>
-            <div class="backButton">
-              <span>
-                <Link to="/">voltar</Link>
-              </span>
-            </div>
-          </div>
-        );
-      }
+  state = {
+    pictures: Array.from({ length: 6 }),
+    hasMore: true
+  };
+
+  fetchMoreData = () => {
+    if (this.state.pictures.length === 18) {
+      this.setState({ hasMore: false });
+      return;
     }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      this.setState({
+        pictures: this.state.pictures.concat(Array.from({ length: 2 }))
+      });
+    }, 100);
+  };
+
+  render() {
+    return (
+      <div>
+          <div id="terrestreTitle">
+            <h2>Terrestre</h2>
+          </div>
+          <InfiniteScroll
+            dataLength={this.state.pictures.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.hasMore}
+            loader={<h4>...</h4>}
+          >
+            {this.state.pictures.map(
+              (i, index) =>
+                index === 0 ? (
+                  (index = null)
+                ) : (
+                  <img src={require(`./terrestre/T${index}.jpg`)} key={index} />
+                )
+            )}
+          </InfiniteScroll>
+          <div className="expoDescription">
+            <a href="#" class="release">
+              release
+            </a>
+          </div>
+        <div className="backButton">
+          <span>
+            <Link to="/">voltar</Link>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Terrestre;
