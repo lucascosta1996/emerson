eb0a8ef5d95aa255f8dcfba4b7dd89e471652d29
Lucas Alves <lucas.alves.costa96@gmail.com>
2018-06-27 22:39:28 -0300

    Update Especie.js

--- src/Especie.js
+++ src/Especie.js
@@ -1,33 +1,61 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 class Especie extends Component {
-  render(){
-    let especiegallery = [
-        "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "E11", "E12", "E13", "E14"
-    ]
-    return(
+  state = {
+    pictures: Array.from({ length: 4 }),
+    hasMore: true
+  };
+
+  fetchMoreData = () => {
+    if (this.state.pictures.length === 18) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      this.setState({
+        pictures: this.state.pictures.concat(Array.from({ length: 2 }))
+      });
+    }, 100);
+  };
+
+  render() {
+    return (
       <div>
-      <section class="galleries">
-        <div id="terrestreTitle">
-        <h2>ESPÉCIES</h2>
-      </div>
-        {especiegallery.map((url, index) => {
-          return <img src={require(`./especie/${url}.jpg`)} key={index}/>
-        })}
-      </section>
-      <div class="expoDescription">
-        <a href="#" class="release">
-          release
-        </a>
-      </div>
-      <div class="backButton">
-        <span>
+          <div id="terrestreTitle">
+            <h2>Espécies</h2>
+          </div>
+          <InfiniteScroll
+            dataLength={this.state.pictures.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.hasMore}
+            loader={<h4>...</h4>}
+          >
+            {this.state.pictures.map(
+              (i, index) =>
+                  index === 0 || index === 18 ? (
+                    (index = null)
+                  ) : (
+                  <img src={require(`./especie/E${index}.jpg`)} key={index} />
+                )
+
+            )}
+          </InfiniteScroll>
+          <div className="expoDescription">
+            <a href="#" class="release">
+              release
+            </a>
+          </div>
+        <div className="backButton">
+          <span>
             <Link to="/">voltar</Link>
-        </span>
+          </span>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
